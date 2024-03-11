e45863f47f745f6d285129117ea4858082e6a398
Marcelo Amaral Alves <marceloaa@live.com>
2024-03-11 17:22:15 -0300

    working on projects horizontal

--- my-new-portfolio/src/Components/Projects/ProjectsCard.jsx
+++ my-new-portfolio/src/Components/Projects/ProjectsCard.jsx
@@ -6,8 +6,8 @@ export const ProjectCard = ({ title, description, imgUrl }) => {
       <div className="box">
         <img src={imgUrl} />
       <div className="proj-txtx">
-          {/* <h4>{title}</h4> */}
-          {/* <span>{description}</span> */}
+          <h4>{title}</h4>
+          <span>{description}</span>
         </div>
       </div>
     </Col>
