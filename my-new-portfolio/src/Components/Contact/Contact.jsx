7ec6196af495f90ad4f38d05f44ca8340d01c677
Marcelo Amaral Alves <marceloaa@live.com>
2024-03-13 09:19:47 -0300

    url

--- my-new-portfolio/src/Components/Contact/Contact.jsx
+++ my-new-portfolio/src/Components/Contact/Contact.jsx
@@ -1,10 +1,10 @@
-import React from 'react'
-import './Contact.css'
+import React, { useRef, useState } from 'react';
+import './Contact.css';
 import emailjs from '@emailjs/browser';
 
-import { useRef } from "react";
 const Contact = () => {
   const form = useRef();
+  const [done, setDone] = useState(false);
 
   const sendEmail = (e) => {
     e.preventDefault();
@@ -12,6 +12,7 @@ const Contact = () => {
     emailjs.sendForm('service_c757fko', 'template_cbwyei9', form.current, 'L3QRb5BHA0HbpW_uc')
       .then((result) => {
         console.log(result.text);
+        setDone(true);
       })
       .catch((error) => {
         console.log(error.text);
@@ -31,6 +32,7 @@ const Contact = () => {
                 <input type="email" name='user_email' className='user' placeholder='Email' />
                 <textarea name="message" className='user' placeholder='Message'></textarea>
                 <input type="submit" value="Send" className="button i-button"/>
+                <span>{done && "Thanks for contacting me!"}</span>
               </form>
             </div>
           </div>
